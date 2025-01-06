f2b54d6c8387548835961ccc721d086e23d7f3a2
Utkarsh Raj <utkarsh.raj1@ofbusiness.in>
2025-01-06 12:57:38 +0530

    initial commit

---
+++ src/main/java/com/JournalNest/JournalNest/JournalNestApplication.java
@@ -0,0 +1,13 @@
+package com.JournalNest.JournalNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JournalNest/JournalNest/JournalNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JournalNest.JournalNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
